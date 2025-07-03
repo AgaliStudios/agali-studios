b19a58da9b97a94dccb33599e851fd2d9feb005f
Thodoris Sairoglou <theodoros.sairoglou@bbv.gr>
2025-07-03 12:54:24 +0300

    Changes for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig(() => {
   return {
+    base: '/agali-studios/',
     plugins: [react()],
     server: {
       open: true,
